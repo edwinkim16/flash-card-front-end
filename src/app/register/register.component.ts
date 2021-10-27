679486b99c55419433f277cf2381aeec394dcfdd
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-10-27 20:55:04 +0300

    register new users done successfully

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserAuthService } from '../services/user-auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  constructor(private authService: UserAuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  // create a new user
+  onSubmit(form: NgForm) {
+    const username = form.value.username;
+    const email = form.value.email;
+    const password = form.value.password;
+    const c_password = form.value.c_password;
+
+    // check if passwords match
+    if (password !== c_password) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    this.authService
+      .createUser(username, email, password)
+      .subscribe((response) => {
+        console.log(response);
+      });
+
+    form.reset();
+
   }
-
 }
